46e6153ff8179fbe16fdc32e4104e62c10f09c43
Great-Emeka <greatestgreatemeka@gmail.com>
2020-10-06 14:07:53 +0100

    created product-tracklisting component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,20 +1,19 @@
-import { Injectable } from '@angular/core';
-import{ Http, Response } from '@angular/http';
-import { Album } from './album';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Album } from "./album";
 
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ProductService {
+  constructor(private _http: Http) {}
 
-  constructor(private _http: Http) { }
+  private _albumUrl = "../assets/album.json";
 
-  private _albumUrl = '../assets/album.json';
-
-  getAlbum(id:number):Observable<Album> {
-    return this._http.get(this._albumUrl)
-    .map(response => <Album>response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http
+      .get(this._albumUrl)
+      .map((response) => <Album>response.json());
   }
-
 }
--- src/app/track.ts
+++ src/app/track.ts
@@ -1,6 +1,3 @@
-
-
-
 export interface Track {
 trackNumber:number;
 trackName:string;
